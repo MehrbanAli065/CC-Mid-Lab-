29d41e326fe015215311e4f26ee7d1ba6dd2da10
MehrbanAli065 <114014769+MehrbanAli065@users.noreply.github.com>
2023-10-26 02:29:02 -0700

    Add files via upload

---
+++ Compiler_Mid_lab(fa20-bcs-065)/Compiler_Mid_lab(fa20-bcs-065)/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Compiler_Mid_lab_fa20_bcs_065_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbInp = new TextBox();
+            tbOut = new TextBox();
+            parse = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            Next = new Button();
+            SuspendLayout();
+            // 
+            // tbInp
+            // 
+            tbInp.Location = new Point(251, 87);
+            tbInp.Multiline = true;
+            tbInp.Name = "tbInp";
+            tbInp.Size = new Size(298, 99);
+            tbInp.TabIndex = 0;
+            // 
+            // tbOut
+            // 
+            tbOut.Location = new Point(251, 283);
+            tbOut.Multiline = true;
+            tbOut.Name = "tbOut";
+            tbOut.Size = new Size(311, 107);
+            tbOut.TabIndex = 1;
+            // 
+            // parse
+            // 
+            parse.BackgroundImageLayout = ImageLayout.Stretch;
+            parse.FlatStyle = FlatStyle.Popup;
+            parse.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            parse.ForeColor = Color.OrangeRed;
+            parse.Location = new Point(332, 225);
+            parse.Name = "parse";
+            parse.Size = new Size(121, 37);
+            parse.TabIndex = 2;
+            parse.Text = "Parse";
+            parse.UseVisualStyleBackColor = true;
+            parse.Click += parse_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(332, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 24);
+            label1.TabIndex = 3;
+            label1.Text = "Qno:2 >>LL1 parsing";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(110, 124);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 26);
+            label2.TabIndex = 4;
+            label2.Text = "Input Text";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(110, 315);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 26);
+            label3.TabIndex = 5;
+            label3.Text = "output text";
+            // 
+            // Next
+            // 
+            Next.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Next.Location = new Point(655, 405);
+            Next.Name = "Next";
+            Next.Size = new Size(106, 33);
+            Next.TabIndex = 6;
+            Next.Text = "Next Form";
+            Next.UseVisualStyleBackColor = true;
+            Next.Click += Next_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Next);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(parse);
+            Controls.Add(tbOut);
+            Controls.Add(tbInp);
+            Name = "Form1";
+            Text = "LL1 Parsing";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbInp;
+        private TextBox tbOut;
+        private Button parse;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button Next;
+    }
+}
---
+++ Compiler_Mid_lab(fa20-bcs-065)/Compiler_Mid_lab(fa20-bcs-065)/Form2.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Compiler_Mid_lab_fa20_bcs_065_
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            tbName = new TextBox();
+            tbLN = new TextBox();
+            tbREG = new TextBox();
+            tbOUT = new TextBox();
+            G_Pass = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(26, 537);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 37);
+            button1.TabIndex = 0;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(299, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(386, 36);
+            label1.TabIndex = 1;
+            label1.Text = "Qno#3 Password generator";
+            // 
+            // tbName
+            // 
+            tbName.Location = new Point(251, 119);
+            tbName.Multiline = true;
+            tbName.Name = "tbName";
+            tbName.Size = new Size(187, 31);
+            tbName.TabIndex = 2;
+            // 
+            // tbLN
+            // 
+            tbLN.Location = new Point(251, 202);
+            tbLN.Multiline = true;
+            tbLN.Name = "tbLN";
+            tbLN.Size = new Size(187, 34);
+            tbLN.TabIndex = 3;
+            // 
+            // tbREG
+            // 
+            tbREG.Location = new Point(251, 289);
+            tbREG.Multiline = true;
+            tbREG.Name = "tbREG";
+            tbREG.Size = new Size(187, 32);
+            tbREG.TabIndex = 4;
+            // 
+            // tbOUT
+            // 
+            tbOUT.Location = new Point(359, 424);
+            tbOUT.Multiline = true;
+            tbOUT.Name = "tbOUT";
+            tbOUT.Size = new Size(283, 106);
+            tbOUT.TabIndex = 5;
+            // 
+            // G_Pass
+            // 
+            G_Pass.BackColor = Color.FromArgb(255, 128, 128);
+            G_Pass.FlatStyle = FlatStyle.Popup;
+            G_Pass.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            G_Pass.ForeColor = Color.Black;
+            G_Pass.Location = new Point(539, 198);
+            G_Pass.Name = "G_Pass";
+            G_Pass.Size = new Size(235, 57);
+            G_Pass.TabIndex = 6;
+            G_Pass.Text = "Generate Password";
+            G_Pass.UseVisualStyleBackColor = false;
+            G_Pass.Click += G_Pass_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(72, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 30);
+            label2.TabIndex = 7;
+            label2.Text = "NAME:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(72, 206);
+            label3.Name = "label3";
+            label3.Size = new Size(138, 30);
+            label3.TabIndex = 8;
+            label3.Text = "LAST NAME:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(85, 291);
+            label4.Name = "label4";
+            label4.Size = new Size(98, 30);
+            label4.TabIndex = 9;
+            label4.Text = "REG NO:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(395, 378);
+            label5.Name = "label5";
+            label5.Size = new Size(212, 30);
+            label5.TabIndex = 10;
+            label5.Text = "Generated Password";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 12F, FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            label6.Location = new Point(551, 157);
+            label6.Name = "label6";
+            label6.Size = new Size(208, 19);
+            label6.TabIndex = 11;
+            label6.Text = "Click here to genrate password";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(958, 586);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(G_Pass);
+            Controls.Add(tbOUT);
+            Controls.Add(tbREG);
+            Controls.Add(tbLN);
+            Controls.Add(tbName);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form2";
+            Text = "Passowrd generator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox tbName;
+        private TextBox tbLN;
+        private TextBox tbREG;
+        private TextBox tbOUT;
+        private Button G_Pass;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
